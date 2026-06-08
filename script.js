cf181535be33bd5aebaabb77bbcbe2cc978e3580
IrfanAnsari21 <irfanansari.career@gmail.com>
2026-06-08 18:44:21 +0530

    Implement lightbox styling and JS functionality

--- script.js
+++ script.js
@@ -24,14 +24,16 @@ backdrop.addEventListener("click", () => {
 
 
 // ----- Gallery -----
-const mainImages = document.querySelectorAll(".gallery .main-image img");
-const thumbnails = document.querySelectorAll(".thumb-list div");
+const mainImages = document.querySelectorAll(".default .main-image img");
+const thumbnails = document.querySelectorAll(".default .thumb-list div");
 
 thumbnails.forEach((thumb, index) => {
     thumb.addEventListener("click", () => {
         changeImage(index, mainImages, thumbnails);
     })
 });
+
+let currentIndex = 0;
 
 function changeImage(index, mainImages, thumbnails) {
 
@@ -45,4 +47,48 @@ function changeImage(index, mainImages, thumbnails) {
 
     mainImages[index].classList.add("active");
     thumbnails[index].classList.add("active");
+
+    currentIndex = index;
 };
+
+
+//----- Lightbox -----
+const lightbox = document.querySelector(".lightbox");
+const lightboxMainImages = document.querySelectorAll(".lightbox .main-image img");
+const lightboxThumbnails = document.querySelectorAll(".lightbox .thumb-list div");
+const closeIcon = document.querySelector(".lightbox .icon-close");
+const prevIcon = document.querySelector(".lightbox .icon-prev");
+const nextIcon = document.querySelector(".lightbox .icon-next");
+
+lightboxThumbnails.forEach((thumb, index) => {
+    thumb.addEventListener("click", () => {
+        changeImage(index, lightboxMainImages, lightboxThumbnails);
+    });
+});
+
+mainImages.forEach((image, index) => {
+    image.addEventListener("click", () => {
+        lightbox.classList.add("active");
+        changeImage(index, lightboxMainImages, lightboxThumbnails);
+    });
+});
+
+prevIcon.addEventListener("click", () => {
+    if (currentIndex <= 0) {
+        changeImage(mainImages.length - 1, lightboxMainImages, lightboxThumbnails);
+    } else {
+        changeImage(currentIndex - 1, lightboxMainImages, lightboxThumbnails);
+    }
+});
+
+nextIcon.addEventListener("click", () => {
+    if (currentIndex >= mainImages.length - 1) {
+        changeImage(0, lightboxMainImages, lightboxThumbnails);
+    } else {
+        changeImage(currentIndex + 1, lightboxMainImages, lightboxThumbnails);
+    }
+});
+
+closeIcon.addEventListener("click", () => {
+    lightbox.classList.remove("active");
+})
